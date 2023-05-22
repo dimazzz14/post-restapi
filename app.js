49131ad9c76e987e8d9582dba8391a4ace007028
dimazzz14 <dimasvalentino14@mhs.mdp.ac.id>
2023-05-22 11:30:00 +0700

    nodemon

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Davai, blin!')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
